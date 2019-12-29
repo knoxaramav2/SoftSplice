991be8782d2d80d638e2a02bc666f95b8af8c6d2
knoxaramav2 <wtscott@iastate.edu>
2019-12-29 14:05:11 -0600

    Splice stubs

--- splice.c
+++ splice.c
@@ -1,1 +1,19 @@
 #include "splice.h"
+
+objectManifest* createManifest(char*dir){
+
+
+    return 0;
+}
+
+void destroyManifest(objectManifest*manifest){
+
+}
+
+void writeManifestLog(objectManifest*manifest, char*outputDir){
+
+}
+
+void manifestRewind(objectManifest*manifest){
+    
+}
--- splice.h
+++ splice.h
@@ -4,6 +4,30 @@
 
 #include "chunk.h"
 
+//File partition information for chunking
+typedef struct objectNode{
+    unsigned start;
+    unsigned length;
+    char * fileName;
 
+    struct objectNod * next;
+} objectNode;
+
+//Define file structure and metadata
+typedef struct objectManifest{
+    objectNode * head;
+    objectNode * current;
+    unsigned files;
+    unsigned totalWidth;
+} objectManifest;
+
+//Create manifest from file/directory
+objectManifest * createManifest(char*);
+//free manifest resources
+void destroyManifest(objectManifest*);
+//write manifest information log
+void writeManifestLog(objectManifest*,char*);
+//Rewind objectManifest->current to head
+void manifestRewind(objectManifest*);
 
 #endif
